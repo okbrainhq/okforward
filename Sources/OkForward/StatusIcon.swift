0d9a8a8278561739be34a77ce89cddfb36a54324
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2026-05-30 07:01:50 -0700

    Add a nice icon

--- Sources/OkForward/StatusIcon.swift
+++ Sources/OkForward/StatusIcon.swift
@@ -15,27 +15,29 @@ enum StatusIcon {
         NSRect(origin: .zero, size: size).fill()
 
         NSColor.black.setStroke()
-        NSColor.black.setFill()
 
-        drawLane(y: 5.5)
-        drawLane(y: 12.5)
+        let lineWidth: CGFloat = 2.5
+
+        // First chevron
+        let chevron1 = NSBezierPath()
+        chevron1.lineWidth = lineWidth
+        chevron1.lineCapStyle = .round
+        chevron1.lineJoinStyle = .round
+        chevron1.move(to: NSPoint(x: 2.5, y: 4))
+        chevron1.line(to: NSPoint(x: 7, y: 9))
+        chevron1.line(to: NSPoint(x: 2.5, y: 14))
+        chevron1.stroke()
+
+        // Second chevron
+        let chevron2 = NSBezierPath()
+        chevron2.lineWidth = lineWidth
+        chevron2.lineCapStyle = .round
+        chevron2.lineJoinStyle = .round
+        chevron2.move(to: NSPoint(x: 8, y: 4))
+        chevron2.line(to: NSPoint(x: 12.5, y: 9))
+        chevron2.line(to: NSPoint(x: 8, y: 14))
+        chevron2.stroke()
 
         return image
-    }
-
-    private static func drawLane(y: CGFloat) {
-        let arrow = NSBezierPath()
-        arrow.lineWidth = 1.8
-        arrow.lineCapStyle = .round
-        arrow.lineJoinStyle = .round
-        arrow.move(to: NSPoint(x: 4.3, y: y))
-        arrow.line(to: NSPoint(x: 12.3, y: y))
-        arrow.move(to: NSPoint(x: 9.8, y: y - 2.3))
-        arrow.line(to: NSPoint(x: 12.5, y: y))
-        arrow.line(to: NSPoint(x: 9.8, y: y + 2.3))
-        arrow.stroke()
-
-        NSBezierPath(ovalIn: NSRect(x: 3, y: y - 1.3, width: 2.6, height: 2.6)).fill()
-        NSBezierPath(ovalIn: NSRect(x: 13, y: y - 1.3, width: 2.6, height: 2.6)).fill()
     }
 }
